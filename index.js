756ff8d59f5bc5d7d3c52e3e002bdf3e2c3c6520
Artur Faruga <Artur.Faruga@ig.com>
2017-07-18 23:43:46 +0200

    changed port number

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http')
 http.createServer(function(request, response) {
 	response.writeHead(200, {"Content-Type": "text/plan"})
 	response.end("\nHello World")
-}).listen(8080);
+}).listen(process.env.PORT);
